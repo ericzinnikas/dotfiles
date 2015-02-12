5733d923bc31853ffbb675544ff72dac08845978
Eric Zinnikas <eric@zinnikas.org>
2015-02-11 20:21:51 -0500

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 #!/bin/sh
+ln -s $PWD/bashrc $HOME/.bashrc
+ln -s $PWD/compton.conf $HOME/.config/compton.conf
+ln -s $PWD/bspwmrc $HOME/.config/bspwm/bspwmrc
+ln -s $PWD/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
