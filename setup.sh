49a2bdf584c770e237eac51d9249ca96ec33155f
Eric Zinnikas <eric@zinnikas.org>
2015-02-11 20:49:13 -0500

    remove absolute dir

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@ ln -s $PWD/bashrc $HOME/.bashrc
 ln -s $PWD/compton.conf $HOME/.config/compton.conf
 ln -s $PWD/bspwmrc $HOME/.config/bspwm/bspwmrc
 ln -s $PWD/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
+ln -s $PWD/fehbg $HOME/.fehbg
+ln -s $PWD/gitconfig $HOME/.gitconfig
+ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD/xinitrc $HOME/.xinitrc
+ln -s $PWD/Xresources $HOME/.Xresources
